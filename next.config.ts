40b32c78a5e6b73170cc895ea791e29e00f0e90c
SjrPovoas <87617524+SjrPovoas@users.noreply.github.com>
2026-05-27 23:24:32 -0300

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  // Esta configuração garante que o Next.js não "roube" a rota /admin
   async rewrites() {
     return [
       {
         source: '/admin',
         destination: '/admin/index.html',
       },
-      {
-        source: '/auth/:path*',
-        destination: 'https://api.github.com/auth/:path*',
-      },
     ];
   },
 };
+
+export default nextConfig;
