51ff9e8411ea5da6786adb2988f5855ab396ab1e
SjrPovoas <87617524+SjrPovoas@users.noreply.github.com>
2026-05-27 23:01:07 -0300

    update config.yml, next.config.ts e public/index.html

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  
-  // Esta configuração garante que o caminho /admin direcione 
-  // para o seu index.html dentro de public/admin/
   async rewrites() {
     return [
       {
         source: '/admin',
         destination: '/admin/index.html',
       },
+      {
+        source: '/auth/:path*',
+        destination: 'https://api.github.com/auth/:path*',
+      },
     ];
   },
 };
-
-export default nextConfig;
